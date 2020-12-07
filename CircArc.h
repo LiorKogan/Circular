06934282ed0ab9d220cae5b53bbe8e006faa13df
Lior Kogan <koganlior1@gmail.com>
2020-12-07 20:56:47 +0200

    Add files via upload

---
+++ CircArc.h
@@ -0,0 +1,230 @@
+// ==========================================================================
+// Copyright (C) 2013 Lior Kogan (koganlior1@gmail.com)
+// ==========================================================================
+// classes defined here:
+// CircArc            - circular arc
+// CircArcTester      - tester for CircArc class
+// ==========================================================================
+
+#pragma once
+
+#include <cmath>
+#include <assert.h>
+
+#include "CircVal.h" // CircVal, CircValTypeDef
+
+// ==========================================================================
+// circular arc length
+// Type should be defined using the CircValTypeDef macro
+template <typename Type>
+class CircArcLen
+{
+    double l; // the arc length [0, Type::R]
+
+public:
+    // ---------------------------------------------
+    operator double() const
+    {
+        return l;
+    }
+
+    // ---------------------------------------------
+    // construction based on a floating-point value
+    // floating-point is truncated into the range [0, Type::R]
+    CircArcLen(double r) : l(__min(__max(0., r), Type::R))
+    {
+    }
+
+    // construction based on a circular arc length of the same type
+    CircArcLen(const CircArcLen& c) : l(c.l)
+    {
+    }
+
+    // construction based on a circular arc length another type
+    // sample use: CircArcLen<SignedRadRange> c= c2;   -or-   CircArcLen<SignedRadRange> c(c2);
+    template<typename Type2>
+    CircArcLen(const CircArcLen<Type2>& c) : l((c == Type2::R) ? Type::R : Type::R/Type2::R * c) // ... to avoid rounding errors
+    {
+    }
+
+    // ---------------------------------------------
+    // assignment from a floating-point value
+    // floating-point is truncated into the range [0, Type::R]
+    CircArcLen& operator= (double r)
+    {
+        l = __min(__max(0., r), Type::R);
+        return *this;
+    }
+
+    // assignment from another type of circular arc length
+    template<typename Type2>
+    CircArcLen& operator= (const CircArcLen<Type2>& c)
+    {
+        l = (c == Type2::R) ? Type::R : Type::R/Type2::R * c; // ... to avoid rounding errors
+        return *this;
+    }
+};
+
+// ==========================================================================
+// circular arc
+// Type should be defined using the CircValTypeDef macro
+template <typename Type>
+class CircArc
+{
+    // the arc [c1,c1+l] is defined by the shortest increasing walk from c1 to c1+l, unless l=Type::R where the arc is defined as the whole circle
+    CircVal   <Type> c1; // the arc start-point [Type::L, Type::H)
+    CircVal   <Type> c2; // the arc end  -point [Type::L, Type::H). Note that c2=c1 in two cases: (a) l=0 (b) l=Type::R
+    CircArcLen<Type> l ; // the arc length      [0      , Type::R]
+
+    // ---------------------------------------------
+public:
+    CircVal   <Type> GetC1() const { return c1; } // the arc start-point [Type::L, Type::H)
+    CircVal   <Type> GetC2() const { return c2; } // the arc end  -point [Type::L, Type::H). Note that c2=c1 in two cases: (a) l=0 (b) l=Type::R
+    CircArcLen<Type> GetL () const { return l ; } // the arc length      [0      , Type::R]
+
+    // ---------------------------------------------
+    CircArc() : c1(Type::Z), c2(Type::Z), l(0)
+    {
+    }
+
+    // "reference" construction:
+    // construction based on CircVal (arc start point) and CircArcLen (arc length)
+    template<typename Type2, typename Type3>
+    CircArc(const CircVal<Type2>& _c1, const CircArcLen<Type3>& _l) : c1(_c1), c2((double)CircVal<Type>(c1)+(double)CircArcLen<Type>(_l)), l(_l)
+    {
+    }
+
+    // construction based on two floating-point values (arc start-point, arc length)
+    // c1 is wrapped into the range, r is truncated into the range
+    CircArc(double fc1, double fl) : c1(fc1), c2(fc1+CircArcLen<Type>(fl)), l(fl)
+    {
+    }
+
+    // construction based on two circular values of same/different type (arc start-point, arc end-point)
+    // note that if c1==c2, the arc length will be 0
+    template<typename Type2, typename Type3>
+    CircArc(const CircVal<Type2>& _c1, const CircVal<Type3>& _c2) : c1(_c1), c2(_c2), l(CircVal<Type>::Pdist(c1, c2))
+    {
+    }
+
+    // construction based on another circular arc of same/different circular-value type
+    // sample use: CircArc<SignedRadRange> a= a2;   -or-   CircArc<SignedRadRange> a(a2);
+    template<typename Type2>
+    CircArc(const CircArc<Type2>& a) : c1(a.GetC1()), c2(a.GetC2()), l(a.GetL())
+    {
+    }
+
+    // ---------------------------------------------
+    // assignment from a circular arc of same/different circular arc type
+    template<typename Type2>
+    CircArc& operator= (const CircArc<Type2>& a)
+    {
+        c1 = a.GetC1();
+        c2 = a.GetC2();
+        l  = a.GetL ();
+        return *this;
+    }
+
+    // ---------------------------------------------
+    bool operator==(const CircArc& a) const
+    {
+        if ( (l == Type::R) && (a.l == Type::R) ) // both are full-circle; start-point doesn't matter
+            return true;
+
+        return (c1 == a.c1) && (l == a.l);
+    }
+
+    bool operator!=(const CircArc& a) const
+    {
+        if ( (l == Type::R) && (a.l == Type::R) ) // both are full-circle; start-point doesn't matter
+            return false;
+
+        return (c1 != a.c1) || (l != a.l);
+    }
+
+    // check if this arc contains a circular value (note that arc contains its endpoints)
+    bool Contains(const CircVal<Type>& c) const
+    {
+        return l - CircVal<Type>::Pdist(c1, c) > -1e-12;
+    }
+
+    // check if this arc contains another circular arc (note that arcs contain their endpoints)
+    bool Contains(const CircArc& a) const
+    {
+        if (  l == Type::R) return true ; // full-circle
+        if (a.l == Type::R) return false; // full-circle
+
+        // ensure order: c1 --- a.c1 --- a.c2 --- c2
+        const double l1= CircVal<Type>::Pdist(c1, a.c1);
+        const double l2= CircVal<Type>::Pdist(c1, a.c2);
+        return (l2 - l1 > -1e-12) && (l - l2 > -1e-12);
+    }
+
+    // check if two circular arcs intersect (note that arcs contain their endpoints)
+    bool Intersect(const CircArc& a) const
+    {
+        return Contains(a.c1) || a.Contains(c1); // one of them contains the start of the other
+    }
+};
+
+// ==========================================================================
+// circular arcs
+// Type should be defined using the CircValTypeDef macro
+template <typename Type>
+class CircArcs
+{
+};
+
+// CircArcs Union       (const CircArc& a);
+// CircArcs Intersection(const CircArc& a);
+// CircArcs Diff        (const CircArc& a);
+
+// ==========================================================================
+// tester for CircVal class
+template <typename Type>
+class CircArcTester
+{
+public:
+    CircArcTester()
+    {
+        Test();
+    }
+
+    static void Test()
+    {
+        const unsigned nSteps = 36              ;
+        const double   fStep  = Type::R / nSteps;
+
+        unsigned m = 0, n = 0, p = 0, q[nSteps+1];
+
+        for (int i = 0; i<nSteps; ++i)
+            q[i] = 0;
+
+        for (int i = 0; i < nSteps; ++i)
+            for (int j = 0; j <= nSteps; ++j)
+            {
+                CircArc<Type> a1(Type::L+i*fStep, j*fStep); // start-point, length
+
+                for (int k = 0; k < nSteps; ++k)
+                    for (int l = 0; l <= nSteps; ++l)
+                    {
+                        CircArc<Type> a2(Type::L+k*fStep, l*fStep); // start-point, length
+
+                        bool b1 = a1.Contains(a2); if (b1) ++m;
+                        bool b2 = a2.Contains(a1); if (b2) ++n;
+
+                        if   (a1 == a2) { assert(  b1 && b2 ); ++p; }
+                        else              assert(!(b1 && b2));
+
+                        if (a1.Intersect(a2)) ++q[j];
+                    }
+            }
+
+        assert (p == 2*nSteps*nSteps                           ); // number of identical arcs
+        assert (m == nSteps*nSteps*(nSteps*nSteps+9*nSteps+8)/6); // number of sub-arcs
+        assert (m == n                                         );
+//      assert (q == ...)                                         // number of intersecting arcs
+
+        // --------------
+    }
+};
---
+++ Circular.cpp
@@ -0,0 +1,317 @@
+// ==========================================================================
+// Copyright (C) 2011 Lior Kogan (koganlior1@gmail.com)
+// ==========================================================================
+
+#include "stdafx.h"
+#include "CircVal.h"
+#include "CircArc.h"
+
+#include <chrono>
+#include <iostream>                 // cout
+#include <fstream>                  // ofstream
+#include <ppl.h>                    // Concurrency::parallel_for
+
+#include "CircVal.h"                // CircVal, CircValTester
+#include "CircStat.h"               // CircAverage, WeightedCircAverage, CAvrgSampledCircSignal, CircMedian
+#include "CircHelper.h"             // Sqr, Mod
+#include "TruncNormalDist.h"        // truncated_normal_distribution
+#include "WrappedNormalDist.h"      // wrapped_normal_distribution
+#include "WrappedTruncNormalDist.h" // wrapped_truncated_normal_distribution
+
+// ==========================================================================
+int _tmain(int argc, _TCHAR* argv[])
+{
+    auto Time0 = chrono::system_clock::now();
+
+    // ------------------------------------------------------
+    {
+        CircVal   <UnsignedDegRange> Start0 ( 10.);
+        CircArcLen<UnsignedRadRange> Length0(M_PI);
+        CircArc   <SignedDegRange  > a0     (Start0, Length0); // construct by CircVal, CircArcLen
+
+        CircVal   <UnsignedRadRange> Start1 (M_PI/2  );
+        CircVal   <UnsignedRadRange> End1   (M_PI/2*3);
+        CircArc   <UnsignedDegRange> a1     (Start1, End1);    // construct by CircVal, CircVal
+
+        CircVal   <UnsignedDegRange> Start2 ( 10.);
+        CircVal   <UnsignedRadRange> End2   (M_PI);
+        CircArc   <SignedDegRange  > a2     (Start2, End2);    // construct by CircVal, CircVal (of different types)
+
+        CircArc   <UnsignedDegRange> a3     (100, 200);        // construct by double, double
+        CircArc   <UnsignedRadRange> a4     (a3);              // construct by CircArc (of different type)
+        a4 = a3;                                               // assignment operator
+
+        bool b1 = a3.Contains( 50 );
+        bool b2 = a3.Contains(100 );
+        bool b3 = a3.Contains(150 );
+        bool b4 = a3.Contains(200 );
+        bool b5 = a3.Contains(250 );
+        bool b6 = a3.Contains(300 );
+        bool b7 = a3.Contains(Start1);
+
+        CircArc<UnsignedDegRange> Arc3(a3);
+        CircArc<UnsignedRadRange> Arc4(a3);
+        CircArc<UnsignedRadRange> Arc5 = a3;
+        Arc5 = a4;
+
+        bool b = (Arc4 == Arc3);
+        double d = Arc4.GetL();
+
+        bool c1 = a4.Contains(Arc3);
+        bool c2 = CircArc<SignedDegRange  >(-170., 360.).Contains (CircArc<SignedDegRange  >(-180., 360.)); // both are full circles
+        bool c3 = CircArc<UnsignedDegRange>(   0., 100.).Intersect(CircArc<UnsignedDegRange>( 100., 100.));
+    }
+
+    // todo: assure ArcLength is equal
+
+    // ------------------------------------------------------
+    // testing correctness of CircVal class implementation
+    {
+        CircValTester<SignedDegRange  > testA;
+        CircValTester<UnsignedDegRange> testB;
+        CircValTester<SignedRadRange  > testC;
+        CircValTester<UnsignedRadRange> testD;
+
+        CircValTester<TestRange0      > test0;
+        CircValTester<TestRange1      > test1;
+        CircValTester<TestRange2      > test2;
+        CircValTester<TestRange3      > test3;
+    }
+
+    // ------------------------------------------------------
+    // testing correctness of CircArc class implementation
+    {
+        CircArcTester<SignedDegRange  > testA;
+        CircArcTester<UnsignedDegRange> testB;
+        CircArcTester<SignedRadRange  > testC;
+        CircArcTester<UnsignedRadRange> testD;
+
+        CircArcTester<TestRange0      > test0;
+        CircArcTester<TestRange1      > test1;
+        CircArcTester<TestRange2      > test2;
+        CircArcTester<TestRange3      > test3;
+    }
+
+    // ------------------------------------------------------
+    // sample code: basic circular math operations
+    {
+        CircVal<SignedDegRange  > c1 =  10. ;
+        CircVal<UnsignedRadRange> c2 =   0.2;
+        CircVal<SignedDegRange  > c3 = c1+c2;
+
+        c1 = c2;
+
+        c1 += 355.;
+        double d = c1;
+
+        d  = sin(c1) / cos(c2) + tan(c3);
+        c1 = asin<SignedDegRange>(0.5); // don't do d1= asin(0.5) !!!
+    }
+
+    // ------------------------------------------------------
+    // sample code: generating wrapped normal distributed random value
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+
+        double fAvrg  =    0.;
+        double fSigma =   45.;
+        double fL     = -180.; // wrapping-range lower-bound
+        double fH     =  180.; // wrapping-range upper-bound
+
+        wrapped_normal_distribution<double> r_wrp(fAvrg, fSigma, fL, fH);
+        double r1 = r_wrp(rand_engine); // random value
+    }
+
+    // ------------------------------------------------------
+    // sample code: generating truncated normal distributed random value
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+
+        double fAvrg  =    0.;
+        double fSigma =   45.;
+        double fA     =  -40.; // truncation-range lower-bound
+        double fB     =   40.; // truncation-range upper-bound
+
+        truncated_normal_distribution<double> r_trn(fAvrg, fSigma, fA, fB);
+        double r2 = r_trn(rand_engine); // random value
+    }
+
+    // ------------------------------------------------------
+    // sample code: generating wrapped truncated normal distributed random value
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+
+        // normal distribution is first truncated, and then wrapped
+
+        const double fAvrg  =    0.;
+        const double fSigma =  100.;
+        const double fA     = -500.; // truncation-range lower-bound
+        const double fB     =  500.; // truncation-range upper-bound
+        const double fL     =    0.; // wrapping  -range lower-bound
+        const double fH     =  360.; // wrapping  -range upper-bound
+
+        wrapped_truncated_normal_distribution<double> r_wrp_trn(fAvrg, fSigma, fA, fB, fL, fH);
+        double d = r_wrp_trn(rand_engine); // random value
+    }
+
+    // ------------------------------------------------------
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+        uniform_real_distribution<double> ud(0., 360.);
+
+        const size_t count = 100;
+        vector<CircVal<UnsignedDegRange>> Angles2(count);
+
+        auto Time0 = chrono::system_clock::now();
+        for (int i = 0; i<100000; i++)
+        {
+            for (int j = 0; j< count; ++j)
+                Angles2[j] = ud(rand_engine);
+
+            auto y= CircAverage (Angles2);
+        }
+        auto Duration = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - Time0).count();
+        cout << Duration << endl;
+
+        Time0 = chrono::system_clock::now();
+        for (int i = 0; i<100000; i++)
+        {
+            for (int j = 0; j< count; ++j)
+                Angles2[j] = ud(rand_engine);
+
+            auto z = CircAverage2(Angles2);
+        }
+        Duration = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - Time0).count();
+        cout << Duration << endl;
+
+        cout << "=================" << endl;
+    }
+
+    // ------------------------------------------------------
+    // code used to collect data for graphs that demonstrate average of circular values
+    {
+        vector<CircVal<UnsignedDegRange>> Angles2 = {30., 130., 230., 330.};
+        auto y = CircAverage (Angles2);
+        auto z = CircAverage2(Angles2);
+
+        ofstream f0("log0.txt");
+
+        for (double x = 0.; x<=360.; x+= 0.1)
+        {
+            double fSum = 0;
+            for (const auto& a : Angles2)
+                fSum += Sqr(__min(abs(x-a), 360.-abs(x-a)));
+
+            f0 << x << "\t" << fSum << endl;
+        }
+    }
+
+    // ------------------------------------------------------
+    // sample code: calculate median, average and weighted-average set of circular-values
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+
+        uniform_real_distribution<double> ud(UnsignedDegRange::L, UnsignedDegRange::H);
+
+        vector<     CircVal<UnsignedDegRange>        > angles1;
+        vector<pair<CircVal<UnsignedDegRange>,double>> angles2;
+
+        for (size_t i = 0; i<3; ++i)
+        {
+            double m = ud(rand_engine);
+            angles1.emplace_back(m     );
+            angles2.emplace_back(m, 0.3);
+        }
+
+        auto Medn  = CircMedian         (angles1);
+        auto Avrg1 = CircAverage        (angles1);
+        auto Avrg2 = WeightedCircAverage(angles2);
+    }
+
+    // ------------------------------------------------------
+    // sample code: estimate average of a sampled continuous-time circular signal, using circular linear interpolation
+    {
+        CAvrgSampledCircSignal<UnsignedDegRange> A1;
+        A1.AddMeasurement(CircVal<UnsignedDegRange>(200.), 1);
+        A1.AddMeasurement(CircVal<UnsignedDegRange>(300.), 2);
+        A1.AddMeasurement(CircVal<UnsignedDegRange>( 20.), 6);
+
+        CircVal<UnsignedDegRange> ad1;
+        A1.GetAvrg(ad1);
+    }
+
+    // ------------------------------------------------------
+    // code used to collect data for RMS error of average estimation based on noisy measurements
+    {
+        std::default_random_engine rand_engine;
+        std::random_device         rnd_device ;
+        rand_engine.seed(rnd_device()); // reseed engine
+
+        ofstream f1("log1.txt");
+
+        Concurrency::parallel_for(1, 101, [&](int nStdDev)  // for each value of standard-deviation
+        {
+            uniform_real_distribution<double> ud(0., 360.);
+
+            double fSumSqrErr1 = 0.;
+            double fSumSqrErr2 = 0.;
+
+            const size_t nTrails  = 50000;                  // number of trails
+            const size_t nSamples =  1000;                  // number of observations per trail
+
+            vector<CircVal<UnsignedDegRange>> vInput(nSamples);
+
+            const double fAvrg = ud(rand_engine);           // our const parameter for this trail
+            wrapped_normal_distribution          <double> r_wnd1(fAvrg, nStdDev,                       0., 360.);
+         // wrapped_truncated_normal_distribution<double> r_wnd1(fAvrg, nStdDev, fAvrg-45., fAvrg+45., 0., 360.);
+
+            for (size_t t = 0; t<nTrails; ++t)
+            {
+                for (auto& Sample : vInput)
+                    Sample = r_wnd1(rand_engine);           // generate "noisy" observation
+
+                set<CircVal<UnsignedDegRange>> sAvrg1 = CircAverage(vInput);                   // avrg - method 1 (new method)
+
+                double fSigSin = 0.;
+                double fSigCos = 0.;
+
+                for (const auto& Sample : vInput)
+                {
+                    fSigSin += sin(Sample);
+                    fSigCos += cos(Sample);
+                }
+
+                CircVal<UnsignedDegRange> Avrg2 = atan2<UnsignedDegRange>(fSigSin, fSigCos);   // avrg - method 2 (conventional method)
+
+                const double fErr1 = CircVal<UnsignedDegRange>::Sdist(*sAvrg1.begin(), fAvrg); // error of estimate - method 1
+                const double fErr2 = CircVal<UnsignedDegRange>::Sdist(Avrg2          , fAvrg); // error of estimate - method 2
+
+                fSumSqrErr1 += Sqr(fErr1);
+                fSumSqrErr2 += Sqr(fErr2);
+            }
+
+            const double fRMS1 = sqrt(fSumSqrErr1 / (nTrails-1));  // root mean square error - method 1
+            const double fRMS2 = sqrt(fSumSqrErr2 / (nTrails-1));  // root mean square error - method 2
+
+            f1 << nStdDev << "\t" << fRMS1 << "\t" << fRMS2 << endl; // save RMS results to file
+        } );
+    }
+
+    // -----------------------------------
+    auto Duration = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - Time0).count();
+    cout << Duration << endl;
+
+    // system ("pause");
+
+    return 0;
+}
